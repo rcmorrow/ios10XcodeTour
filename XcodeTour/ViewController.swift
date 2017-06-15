4ab6d6fd8aae450632a47d75480fd0662e3db74f
Robert Morrow <rcmorrow01@verizon.net>
2017-06-15 15:12:16 -0400

    Initial Commit

---
+++ XcodeTour/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XcodeTour
+//
+//  Created by Robert Morrow on 6/15/17.
+//  Copyright © 2017 Robert Morrow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
